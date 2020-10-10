4435c2a976ec132ed23e63df3a3a2fba71e01f3c
gabliw <hchan11@naver.com>
2020-10-10 22:55:09 +0900

    pytorch reference code update

--- Code/construct.py
+++ Code/construct.py
@@ -1,51 +1,123 @@
-from __future__ import print_function
-import os
-import random
-import argparse
+"""
+HShake (https://github.com/gabliw)
+thanks to winterchild
+"""
 
+import os
+import os.path
+import argparse
+import numpy as np
 import torch
 import torch.nn as nn
-import torch.backends.cudnn as cudnn
 import torch.optim as optim
-
-import torchvision
-import torchvision.datasets as datasets
+import torch.utils.data as loader
+import torch.utils.tensorboard as tensorboard
+import torchvision.models as models
 import torchvision.transforms as transforms
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default='imagenet', help='cifar10 | celebA | mnist |imagenet | folder | lfw | fake')
-parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
-parser.add_argument('--param', type=str, default='base', help='parameter json file load')
-
-opt = parser.parse_args()
-print(opt)
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# import datasets
+# import modules
+import config
+import utils
 
 
-# We can use an image folder dataset the way we have it setup.
-# Create the dataset
-dataset = datasets.ImageFolder(root='../Dataset/celeba',
-                           transform=transforms.Compose([
-                               transforms.Resize(image_size),
-                               transforms.CenterCrop(image_size),
-                               transforms.ToTensor(),
-                               transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                           ]))
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-breakpoint()
+parser = argparse.ArgumentParser(description="Gait Analysis Project")
+parser.add_argument('--json', type=str, default="model config", help='collector file')
+parser.add_argument('--Header', type=str, default="not used...", help='output header')
+parser.add_argument('--epochs', type=int, default=20, help='epochs default=20')
+parser.add_argument('--save', type=bool, default=True, help='save state default=True')
+args = parser.parse_args()
 
 
-# Create the dataloader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+def main(args):
 
-# Decide which device we want to run on
-device = torch.device("cuda:0" if (torch.cuda.is_available() and ngpu > 0) else "cpu")
+    # best_score = np.inf
 
-# Plot some training images
-real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
-plt.axis("off")
-plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    # transform = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)) # imageNet norm
+    # ])
+
+    # trainset = datasets.RTGENE(root=args.root, transform=transform, subjects=args.trainlist, data_type=args.data_type)
+    # validset = datasets.RTGENE(root=args.root, transform=transform, subjects=args.validlist, data_type=args.data_type)
+    # trainloader = loader.DataLoader(trainset, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    # validloader = loader.DataLoader(validset, batch_size=args.batch_size, shuffle=False, num_workers=args.workers)
+
+    model = models.resnet50(pretrained=True)
+    model.fc = nn.Linear(in_features=model.fc.in_features, out_features=2)
+    model.to(args.device)
+
+    criterion = nn.MSELoss()
+    # evaluator = modules.AngleError()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=args.betas)
+    # scheduler -> optimizer lr scheduler
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, cooldown=3)
+
+    # writer = tensorboard.SummaryWriter(os.path.join(args.logs, 'temp1'))
+
+    for epoch in range(args.epochs):
+        args.epoch = epoch
+        # train(trainloader, model, criterion, evaluator, optimizer, None, args)
+        # score = validate(validloader, model, criterion, evaluator, None, args)
+        # scheduler.step(score)
+        # writer.add_scalar('learning rate', optimizer.param_groups[0]['lr'], epoch)
+
+        # is_best = score < best_score
+        # best_score = min(score, best_score)
+        # utils.save_checkpoint(model, is_best, args.save)
+
+    # writer.close()
+
+
+def train(dataloader, model, criterion, evaluator, optimizer, writer, args):
+
+    model.train()
+    for i, batch in enumerate(dataloader):
+        face, _, gaze = batch
+        face, gaze = face.to(args.device), gaze.to(args.device)
+
+        outputs = model(face)
+        loss = criterion(outputs, gaze)
+        score = evaluator(outputs, gaze)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # writer.add_scalar('training loss', loss.item(), args.epoch * len(dataloader) + i)
+        # writer.add_scalar('training score', score.item(), args.epoch * len(dataloader) + i)
+
+        print(f'Epoch[{args.epoch + 1:4d}/{args.epochs:4d}] - batch[{i + 1:4d}/{len(dataloader):4d}]'
+              f' - loss: {loss.item():7.3f} - accuracy: {score.item():7.3f}')
+
+
+def validate(dataloader, model, criterion, evaluator, writer, args):
+
+    res = []
+    model.eval()
+    with torch.no_grad():
+        for i, batch in enumerate(dataloader):
+            face, _, gaze = batch
+            face, gaze = face.to(args.device), gaze.to(args.device)
+
+            outputs = model(face)
+            loss = criterion(outputs, gaze)
+            score = evaluator(outputs, gaze)
+
+            res.append(loss.item())
+            res.append(score.item())
+
+            # writer.add_scalar('validation loss', loss.item(), args.epoch * len(dataloader) + i)
+            # writer.add_scalar('validation score', score.item(), args.epoch * len(dataloader) + i)
+
+            print(f'Epoch[{args.epoch + 1:4d}/{args.epochs:4d}] - batch[{i + 1:4d}/{len(dataloader):4d}]'
+                  f' - loss: {loss.item():7.3f} - accuracy: {score.item():7.3f}')
+
+    return np.nanmean(res)
+
+
+if __name__ == '__main__':
+    # main(config.ConfigParser('config/config.json'))
+    main(None)
--- Code/utils.py
+++ Code/utils.py
@@ -1,4 +1,9 @@
+"""
+HShake (https://github.com/gabliw)
+"""
+
 import os
+import json
 
 
 # image file rename function
@@ -12,4 +17,80 @@ def rename(target_dir):
         os.rename(os.path.join(target_dir, file), os.path.join(target_dir, file_name))
 
 
-rename('/Users/noble/workspace/NaNet/Dataset/target')
+# Contributed with Winterchild
+class _Dict(dict):
+
+    def __init__(self, *args, **kwargs):
+        super(_Dict, self).__init__(*args, **kwargs)
+        for k, v in self.items():
+            if type(v) is dict:
+                self[k] = _Dict(v)
+
+    def __getattr__(self, key):
+        def __proxy__(_dict, key):
+            for k, v in _dict.items():
+                if k == key:
+                    return v
+                if isinstance(v, _Dict):
+                    ret = __proxy__(v, key)
+                    if ret is not None:
+                        return ret
+                    else:
+                        continue
+        try:
+            return __proxy__(self, key)
+        except KeyError as e:
+            raise AttributeError(e)
+
+    def __setattr__(self, key, value):
+        def __proxy__(_dict, key, value):
+            for k in _dict.keys():
+                if k == key:
+                    _dict[k] = value
+                    return True
+                if isinstance(_dict[k], _Dict):
+                    return __proxy__(_dict[k], key, value)
+        if __proxy__(self, key, value):
+            return
+        self[key] = value
+
+    def __delattr__(self, key):
+        def __proxy__(_dict, key):
+            for k in _dict.keys():
+                if k == key:
+                    del _dict[k]
+                    return
+                if isinstance(_dict[k], _Dict):
+                    __proxy__(_dict[k], key)
+        try:
+            __proxy__(self, key)
+        except KeyError as e:
+            raise AttributeError(e)
+
+
+class ConfigParser(_Dict):
+
+    def __init__(self, filename):
+        self.filename = os.path.abspath(filename)
+        super(ConfigParser, self).__init__(self.read_configs())
+
+    def read_configs(self):
+        with open(self.filename) as f:
+            configs = json.load(f)
+        return configs
+
+    def initialize_object(self, name, module, *args, **kwargs):
+        module_name = self[name]['type']
+        module_args = dict(self[name]['args'])
+        assert all([k not in module_name for k in kwargs])
+        module_args.update(kwargs)
+        return getattr(module, module_name)(*args, **module_args)
+
+
+def print_json(config, indent=''):
+    for k, v in config.items():
+        if type(config[k]) is _Dict:
+            print(indent+f'{k}:')
+            print_json(v, indent+'    ')
+        else:
+            print(indent+f'{k}: {v}')
